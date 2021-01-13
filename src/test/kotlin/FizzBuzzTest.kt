44fcb680d8991da0baceadad264f7c6b5c9bdeb6
David Garcia Latorre <dgarcia@seocom.agency>
2021-01-13 18:52:37 +0100

    Test refactor. Want to remove the ifs inside class to maintain solid OpenClose principle in next steps

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -1,68 +1,48 @@
 import org.kata.fizzbuzz.FizzBuzz
 import org.testng.Assert
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 
 class FizzBuzzTest {
+    private var actor = FizzBuzz()
+
+    @BeforeClass
+    fun globalSetUp() {
+        this.actor = FizzBuzz()
+    }
+
     @Test
     fun `test obtain Fizz word when number is divisible by 3`() {
-        val actor = FizzBuzz()
-
-        val result = actor.result(3)
-
-        Assert.assertEquals(result, "Fizz")
+        Assert.assertEquals(actor.result(3), "Fizz")
     }
 
     @Test
     fun `test obtain Buzz word when number is divisible by 5`() {
-        val actor = FizzBuzz()
-
-        val result = actor.result(5)
-
-        Assert.assertEquals(result, "Buzz")
+        Assert.assertEquals(actor.result(5), "Buzz")
     }
 
     @Test
     fun `test a number not in fizzbuzz rules return the same number`() {
-        val actor = FizzBuzz()
-
-        val result = actor.result(1)
-
-        Assert.assertEquals(result, "1")
+        Assert.assertEquals(actor.result(1), "1")
     }
 
     @Test
     fun `test obtain Fizz for numbers 3, 6, 9`() {
-        val actor = FizzBuzz()
-
-        val result1 = actor.result(3)
-        val result2 = actor.result(6)
-        val result3 = actor.result(9)
-
-        Assert.assertEquals(result1, "Fizz")
-        Assert.assertEquals(result2, "Fizz")
-        Assert.assertEquals(result3, "Fizz")
+        Assert.assertEquals(actor.result(3), "Fizz")
+        Assert.assertEquals(actor.result(6), "Fizz")
+        Assert.assertEquals(actor.result(9), "Fizz")
     }
 
     @Test
     fun `test obtain Buzz for numbers 5,10,20`() {
-        val actor = FizzBuzz()
-
-        val result1 = actor.result(5)
-        val result2 = actor.result(10)
-        val result3 = actor.result(20)
-
-        Assert.assertEquals(result1, "Buzz")
-        Assert.assertEquals(result2, "Buzz")
-        Assert.assertEquals(result3, "Buzz")
+        Assert.assertEquals(actor.result(5), "Buzz")
+        Assert.assertEquals(actor.result(10), "Buzz")
+        Assert.assertEquals(actor.result(20), "Buzz")
     }
 
     @Test
     fun `test obtain FizzBuzz word when number is divisible by 3 and 5`() {
-        val actor = FizzBuzz()
-
-        val result = actor.result(15)
-
-        Assert.assertEquals(result, "FizzBuzz")
+        Assert.assertEquals(actor.result(15), "FizzBuzz")
     }
 
 }
