72c5b7cc737baa2b9d3421c31ba963e8b77126fd
David Garcia Latorre <dgarcia@seocom.agency>
2021-01-13 19:10:05 +0100

    Making room for the new DividerBy classes using Parallel change technique

--- src/main/kotlin/org/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/org/kata/fizzbuzz/FizzBuzz.kt
@@ -1,7 +1,14 @@
 package org.kata.fizzbuzz
 
-class FizzBuzz(dividers: List<DividerBy>) {
+class FizzBuzz(val dividers: List<DividerBy>) {
     fun result(i: Int): String {
+
+        for( divider in this.dividers) {
+            if ( divider.applyTo(i) ) {
+                return divider.result()
+            }
+        }
+
         if (0 == i.rem(3) && 0 == i.rem(5)) {
             return "FizzBuzz"
         }
