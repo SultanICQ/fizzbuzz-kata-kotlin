2cfccdfa5aafec546e0bb5a2f100e94bf7ec1e80
David Garcia Latorre <dgarcia@seocom.agency>
2021-01-13 18:28:27 +0100

    Case FizzBuzz

--- src/main/kotlin/org/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/org/kata/fizzbuzz/FizzBuzz.kt
@@ -2,14 +2,15 @@ package org.kata.fizzbuzz
 
 class FizzBuzz {
     fun result(i: Int): String {
+        if (0 == i.rem(3) && 0 == i.rem(5)) {
+            return "FizzBuzz"
+        }
         if (0 == i.rem(3)) {
             return "Fizz"
         }
-
         if (0 == i.rem(5)) {
             return "Buzz"
         }
-
         return i.toString()
     }
 
--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -21,7 +21,6 @@ class FizzBuzzTest {
         Assert.assertEquals(result, "Buzz")
     }
 
-
     @Test
     fun `test a number not in fizzbuzz rules return the same number`() {
         val actor = FizzBuzz()
@@ -29,6 +28,41 @@ class FizzBuzzTest {
         val result = actor.result(1)
 
         Assert.assertEquals(result, "1")
+    }
+
+    @Test
+    fun `test obtain Fizz for numbers 3, 6, 9`() {
+        val actor = FizzBuzz()
+
+        val result1 = actor.result(3)
+        val result2 = actor.result(6)
+        val result3 = actor.result(9)
+
+        Assert.assertEquals(result1, "Fizz")
+        Assert.assertEquals(result2, "Fizz")
+        Assert.assertEquals(result3, "Fizz")
+    }
+
+    @Test
+    fun `test obtain Buzz for numbers 5,10,20`() {
+        val actor = FizzBuzz()
+
+        val result1 = actor.result(5)
+        val result2 = actor.result(10)
+        val result3 = actor.result(20)
+
+        Assert.assertEquals(result1, "Buzz")
+        Assert.assertEquals(result2, "Buzz")
+        Assert.assertEquals(result3, "Buzz")
+    }
+
+    @Test
+    fun `test obtain FizzBuzz word when number is divisible by 3 and 5`() {
+        val actor = FizzBuzz()
+
+        val result = actor.result(15)
+
+        Assert.assertEquals(result, "FizzBuzz")
     }
 
 }
